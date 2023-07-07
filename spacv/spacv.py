4120df6bba5a9c10dbb88655bf108f54c229fea7
djampar <98717788+djampar@users.noreply.github.com>
2023-07-07 23:51:19 +0200

    Update spacv.py
    change the coordinate system to EASE-Grid 2.0: https://doi.org/10.3390/ijgi1010032 for calculation of the buffer

--- spacv/spacv.py
+++ spacv/spacv.py
@@ -363,7 +363,15 @@ class UserDefinedSCV(BaseSpatialCV):
             # Remove empty grids
             if len(test_indices) < 1:
                 continue
-            grid_poly_buffer = grid.loc[[grid_id]].buffer(self.buffer_radius)
+            # change the coordinate system to EASE-Grid 2.0: https://doi.org/10.3390/ijgi1010032 for calulation of the buffer
+            epsg = grid.crs.to_epsg()
+            if epsg == 4326:
+                grid = grid.to_crs(3699)
+                grid_poly_buffer = grid.loc[[grid_id]].buffer(self.buffer_radius)
+                grid_poly_buffer = grid_poly_buffer.to_crs(epsg)
+                grid = grid.to_crs(epsg)  # return to original EPSG
+            else:
+                grid_poly_buffer = grid.loc[[grid_id]].buffer(self.buffer_radius)
             test_indices, train_exclude = super()._remove_buffered_indices(
                 XYs, test_indices, self.buffer_radius, grid_poly_buffer
             )
